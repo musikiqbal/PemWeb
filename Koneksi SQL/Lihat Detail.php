45b9129fd514e497f04926deec6f3008bc0b640a
musikiqbal <musikiqbal@gmaile.com>
2020-09-04 10:43:58 +0700

    Lihat Detail

---
+++ Koneksi SQL/Lihat Detail.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'Functions.php';
+$mahasiswa = query("SELECT * FROM data_mahasiswa");
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+  <table border="1" cellpadding="5" cellspacing="0">
+    <tr>
+      <th>No</th>
+      <th>Gambar</th>
+      <th>NAMA</th>
+      <th>AKSI</th>
+    </tr>
+    <?php $i = 1;
+    foreach ($mahasiswa as $m) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><img src="Image/<?= $m['Gambar']; ?>" width="60"></td>
+        <td><?= $m['Nama']; ?></td>
+        <td>
+          <a href="detail.php?id=<?= $m['id']; ?>">Lihat Detail</a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+    <table>
+</body>
+
+</html>
